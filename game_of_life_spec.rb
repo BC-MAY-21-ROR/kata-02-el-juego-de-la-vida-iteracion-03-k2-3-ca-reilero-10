fe81bdd69820a8fbe584031772915def8b458e92
Jorge Rodriguez <jarf4321@gmail.com>
2022-08-12 18:09:37 -0500

    Add: New test for Class initialize method
    Co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>
    Co-authored-by: fahida rios <fahidarios@gmail.com>

--- game_of_life_spec.rb
+++ game_of_life_spec.rb
@@ -2,9 +2,17 @@ require 'rspec'
 require_relative 'game_of_life'
 
 describe Grid do
-  describe 'say_hi' do
-    it 'return string message hi' do
-      expect(Grid.say_hi).to eq('hi')
+  elGrid = Grid.new('./generacion_1.txt')
+
+  describe "Create a new grid class" do
+    it 'return a new Grid class' do
+      expect(elGrid).to eq(Grid.new('./generacion_1.txt'))
     end
   end
+
+  # describe 'initialize' do
+  #   it 'return string message hi' do
+  #     expect().to eq('hi')
+  #   end
+  # end
 end
